d4231ee89fea52e3bd96addef4c97d37e62eb772
Adam-Borisoff <adamborisoff@gmail.com>
2015-06-04 12:38:00 -0400

    Create Run.java

---
+++ src/com.adam.game/Run.java
@@ -0,0 +1,12 @@
+package com.adam.game;
+
+import java.io.IOException;
+
+public class Run {
+	protected static SettingsWindow f;
+	public static void main(String[] args)
+		    throws IOException
+		  {
+		  f= new SettingsWindow();
+		  }
+}
